2dd6607a6602e892886b87408e560ed0da1c73ae
wry5560 <233089043@qq.com>
2018-12-25 09:26:04 +0800

    antd-vue 页面项目模版

---
+++ demo-page/precache-manifest.2ede6f24f93e35ec527f5c6e6479a79c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/asrsajj/AS_SYSTEM/demo-page/robots.txt"
+  },
+  {
+    "revision": "bbbec0450cd4cd3cb794",
+    "url": "/asrsajj/AS_SYSTEM/demo-page/js/chunk-vendors.71ea9e72.js"
+  },
+  {
+    "revision": "646e37c476897665dc16",
+    "url": "/asrsajj/AS_SYSTEM/demo-page/js/app.ce9df224.js"
+  },
+  {
+    "revision": "a6b0aea27f5690bfedd5",
+    "url": "/asrsajj/AS_SYSTEM/demo-page/js/about.d7a66b4b.js"
+  },
+  {
+    "revision": "faedce141b153451a37978184ae5ba7e",
+    "url": "/asrsajj/AS_SYSTEM/demo-page/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/asrsajj/AS_SYSTEM/demo-page/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "646e37c476897665dc16",
+    "url": "/asrsajj/AS_SYSTEM/demo-page/css/app.d8d645d5.css"
+  }
+];
